f17582e79fb7a7c9fed38bd003757ace5b8d8f12
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-11-29 21:55:38 +0530

    Middlewrae

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const http = require('http');
 const express = require("express");
-const PORT = 3001;
-
+const bodyParser = require('body-parser');
+const PORT = 3000;
 
 const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+// mimic the db using array
+let blogList = [];
+
+app.get('/blogs', (req,res) => {
+	return res.status(200).json({
+		data: blogList,
+		success:true
+	});
+});
+
+app.post('blogs', () => {
+	console.log(req.body)
+});
 
 app.listen(PORT, () => {
    console.log(`Server is running on PORT: ${PORT}`);
